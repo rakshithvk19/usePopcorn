94f38c083f5baf4514b6a33ac9f8362d84cfe7d1
Rakshith Rajkumar <rajkumar.rak@northeastern.edu>
2023-09-19 11:29:08 -0400

    resolved movie summary bug

--- src/components/WatchedList.js
+++ src/components/WatchedList.js
@@ -32,7 +32,7 @@ export function MovieSummary({ watched }) {
         </p>
         <p>
           <span>⏳</span>
-          <span>{avgRuntime} min</span>
+          <span>{avgRuntime.toFixed(2)} min</span>
         </p>
       </div>
     </div>
